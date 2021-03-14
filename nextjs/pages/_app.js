6d08a33b334313804b765edc726ed969e4271a88
akshat46 <akshatsharma46@gmail.com>
2021-03-14 06:24:07 -0700

    fix[nextjs]: cognito user pool region & fields fix
    - Removed phone number field from cognito sign up ui.
    - amplify custom options to use us-west-1 region (has rest of the
      functions) which was not supported by default.

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -3,7 +3,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
 import Amplify, { Auth } from "aws-amplify";
 import awsconfig from "../src/aws-exports";
-Amplify.configure(awsconfig);
+Amplify.configure({
+    Auth: {
+        region: "us-west-1",
+        userPoolId: "us-west-1_AlfXnfniu",
+        userPoolWebClientId: "2qkf9sqqlf5ms7hcup7vn7mgev",
+    },
+});
 
 function MyApp({ Component, pageProps }) {
     return (
--- nextjs/pages/authentication.js
+++ nextjs/pages/authentication.js
@@ -1,9 +1,33 @@
-import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
-const Authentication = () => (
-    <div>
-        <AmplifySignOut />
-        My App
-    </div>
-);
+import { useState, useEffect } from "react";
+import { withAuthenticator, AmplifyAuthenticator, AmplifySignUp, AmplifySignOut } from "@aws-amplify/ui-react";
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
 
-export default withAuthenticator(Authentication);
+// src to hide email,phone fields for signup:
+// https://docs.amplify.aws/ui/auth/authenticator/q/framework/react#hiding-form-fields
+const Authentication = () => {
+    const [authState, setAuthState] = useState();
+    const [user, setUser] = useState();
+
+    useEffect(() => {
+        return onAuthUIStateChange((nextAuthState, authData) => {
+            setAuthState(nextAuthState);
+            setUser(authData);
+        });
+    }, []);
+
+    return authState === AuthState.SignedIn && user ? (
+        <div className="App">
+            <div>Hello, {user.username}</div>
+            <AmplifySignOut />
+        </div>
+    ) : (
+        <AmplifyAuthenticator>
+            <AmplifySignUp
+                slot="sign-up"
+                formFields={[{ type: "username" }, { type: "password" }, { type: "email" }]}
+            />
+        </AmplifyAuthenticator>
+    );
+};
+
+export default Authentication;
